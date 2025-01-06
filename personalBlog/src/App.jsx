6f15a29e5d061e6442ff8d7d8ab6b56cf605aa4b
Anil Kumar <avulanil17@gmail.com>
2025-01-06 20:31:20 +0530

    create profile page

--- personalBlog/src/App.jsx
+++ personalBlog/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState,useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import { auth } from "./config/firebase";
@@ -17,66 +17,98 @@ import Home from "./Components/Home";
 import Work from "./Components/Work";
 import Post from "./DataFiles/Post";
 import CreateProfile from "./DataFiles/CreateProfile";
-import {db} from './config/firebase'
-import { collection,doc,getDoc, getDocs } from "firebase/firestore";
+import { db } from "./config/firebase";
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
 
 function App() {
-  const signOutRef = useRef (null);
+  const signOutRef = useRef(null);
+  const [proData, setProData] = useState(false);
+  const navigate = useNavigate ();
 
-  const [hasAccount,setHasAccount] = useState (false);
-  const [proData,setProData] = useState (false)
-  
-  useEffect (()=> {
-    const checkProfile = async ()=> {
-      const hasProfileRef = doc (db,"HasProfile","pSSXxyOkJVj7DUbFouyZ");
-      const data = await getDoc (hasProfileRef);
-      const filteredData = data.data ();
-      setProData (filteredData.hasAccount)
-    };
+  const [hasAccount, setHasAccount] = useState(false);
+  const [accountId, setAccountId] = useState("");
+  const [accDetails, setAccDetails] = useState({});
 
-    checkProfile ();
-  },[])
-  
+  useEffect(() => {
+    async function getProfile() {
+      const userRef = collection(db, "HasProfile");
+      const fetchData = await getDocs(userRef);
+      fetchData.forEach((doc) => {
+        const aid = doc.id;
+
+        setAccountId(doc.id);
+        console.log(accountId)
+        const data = doc.data();
+        if (data.hasProfile != undefined) {
+          setHasAccount(data.hasProfile);
+        }
+      });
+    }
+    getProfile();
+  }, []);
+
   const Title = () => {
     return <header className="title">Wander & wonder</header>;
   };
 
-  useEffect (()=> {
+  const handleCreateBlog = ()=> {
+    if (hasAccount) {
+      navigate ("/creat-post")
+    } else {
+      navigate ("/profile")
+    }
+  }
 
-  },[signOutRef]);
+  useEffect(() => {}, [signOutRef]);
 
   const SignOutPopup = () => {
-    return <div ref={signOutRef} className="popupCard" style={{display:'none'}}>
+    return (
+      <div ref={signOutRef} className="popupCard" style={{ display: "none" }}>
         <div className="popup">
           <span>Signed out</span>
-          <button onClick={()=>signOutRef.current.style.display = 'none'}>X</button>
+          <button onClick={() => (signOutRef.current.style.display = "none")}>
+            X
+          </button>
         </div>
       </div>
+    );
   };
-  
+
+  const CreateBlog = ()=>{
+    return <button className="createBlog" onClick={handleCreateBlog}>
+      <div className="sign">+</div>
+      <div className="text">Create</div>
+  </button>
+  }
 
   return (
     <>
       <Title />
       <Router>
-        <Navbar props={{
-          signOutRef:signOutRef,
-          proData:proData,
-          setProData : setProData
-        }}/>
+        <Navbar
+          props={{
+            signOutRef: signOutRef,
+            proData: proData,
+            setProData: setProData,
+          }}
+        />
         <Routes>
           {/* <Route path="/" /> */}
-          <Route path="/" element={<Navigate to="/home"/>}/>
-          <Route path="/profile" element={<Profile setProData={setProData} proData={proData}/>} />
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route
+            path="/profile"
+            element={<Profile accountId={accountId} hasAccount={hasAccount} setHasAccount={setHasAccount} />}
+          />
           <Route path="/work" element={<Work />} />
-          <Route path="/home" element={<Home/>}/>
+          <Route path="/home" element={<Home />} />
           <Route path="/Eat" element={<Eat />} />
           <Route path="/Travel" element={<Travel />} />
           <Route path="/About" element={<About />} />
-          <Route path="/creat-post" element={<Post/>} />
+          <Route path="/creat-post" element={<Post />} />
         </Routes>
       </Router>
-      <SignOutPopup/>
+      <SignOutPopup />
+      <CreateBlog/>
     </>
   );
 }
--- personalBlog/src/Components/Profile.jsx
+++ personalBlog/src/Components/Profile.jsx
@@ -1,48 +1,78 @@
-import React, { useRef } from 'react'
-import './Profile.css'
+import React, { useEffect, useRef, useState } from "react";
+import "./Profile.css";
+import { db } from "../config/firebase";
+import {
+  doc,
+  updateDoc,
+  getFirestore,
+  getDoc,
+  collection,
+  getDocs,
+} from "firebase/firestore";
 
 const Profile = (props) => {
+  const createRef = useRef(null);
+  const profileRef = useRef(null);
+  const [userName,setUserName] = useState ("");
+  const [password,setPassword] = useState ("");
 
-  const createRef = useRef (null);
-  const profileRef = useRef (null);
+  const handleSave = async () => {
+    // console.log(props.proData)
+    console.log(props.accountId, props.hasAccount);
+    // props.setProData(true);
+    let newData = { hasAccount: "true" };
+    const userRef = doc(db, "HasProfile", props.accountId);
+    await updateDoc(userRef, newData)
+      .then(() => console.log("Doc updated sucessfull"))
+      .catch((err) => console.log("error :", err));
 
-  const handleSave = ()=> {
-    console.log(props.proData.hasAccount)
-    if (!props.proData) {
-      props.setProData (true)
-      createRef.current.style.display = 'none';
-      profileRef.current.style.display = 'block';
-    }
+    console.log("its happening", props.accountId);
+    props.setHasAccount((prev) => (prev = true));
+    createRef.current.style.display = "none";
+    profileRef.current.style.display = "block";
+    console.log(props.accountId, props.hasAccount);
   };
 
+  const LoginPageComp = () => {
+    return (
+      <div className="profile" ref={profileRef} style={{ display: "none" }}>
+        <div className="profileData">
+          <img src="./assets/user.png" alt="error" />
+          <h3>Anil Kumar</h3>
+        </div>
+      </div>
+    );
+  };
+
+  const UserProfileComp = () => {
+    return (
+      <div className="create" ref={createRef}>
+        <h1>Create Your Profile</h1>
+        <div className="setProfile">
+          <div className="mainData">
+            <span>
+              <input type="file" />
+              <button>↑ Upload</button>
+            </span>
+            <input className="UserName" type="text" placeholder="Username" minLength='4'
+             onChange={(e)=>setUserName (e.target.value)}/>
+          </div>
+          <input className="desc" type="text" placeholder="description" minLength='10' 
+          onChange={(e)=>setPassword (e.target.value)}/>
+          <button className="saveData" onClick={handleSave}>
+            Save
+          </button>
+        </div>
+      </div>
+    );
+  };
 
   return (
-  <div className='userProfile'>
-
-    <div className='profile' ref={profileRef} style={{display:'none'}}>
-      <div className="profileData">
-        <img src="./assets/user.png" alt="error" />
-        <h3>Anil Kumar</h3>
-      </div>
+    <div className="userProfile">
+      <LoginPageComp />
+      <UserProfileComp />
     </div>
+  );
+};
 
-    <div className="create" ref={createRef}>
-        <h1>Create Your Profile</h1>
-      <div className="setProfile">
-        <div className="mainData">
-          <span>
-            <input type="file" />
-            <button>↑ Upload</button>
-          </span>
-          <input className="UserName" type="text" placeholder="Username" />
-        </div>
-        <input className="desc" type="text" placeholder="description" />
-        <button className="saveData" onClick={handleSave}>Save</button>
-      </div>
-    </div>
-    
-  </div>
-  )
-}
-
-export default Profile
+export default Profile;
